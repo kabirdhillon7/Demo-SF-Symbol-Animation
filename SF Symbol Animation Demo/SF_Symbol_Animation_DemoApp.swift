585af30fa200acee934887c52740d1843770e42f
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-10-11 22:06:37 -0700

    Initial Commit

---
+++ SF Symbol Animation Demo/SF_Symbol_Animation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_Symbol_Animation_DemoApp.swift
+//  SF Symbol Animation Demo
+//
+//  Created by Kabir Dhillon on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SF_Symbol_Animation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
